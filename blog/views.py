eac05630efb7a9315544df18df2b6bd9b08d737e
Roadman-Lee <rdmshine@gmail.com>
2022-02-02 01:47:52 +0900

    create : post_draft_list.html, 게시되지 않은 블로그 글 목록 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -19,7 +19,7 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            # post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -33,9 +33,15 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            # post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    #(published_date__isnull=True) 코드로 발행되지 않은 글 목록을 가지고온다.
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
