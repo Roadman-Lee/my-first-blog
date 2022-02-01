f2abf7b7d16520df8103db25e8990408dbb8bba3
Roadman-Lee <rdmshine@gmail.com>
2022-02-01 14:21:18 +0900

    update : website/settings.py ALLOWED_HOSTS code

--- website/settings.py
+++ website/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-30s0lq=6u(*atj3sphvo*j*aqjrp1$=(3ihu91ynyfpx&z^1fg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
